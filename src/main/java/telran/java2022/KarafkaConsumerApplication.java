898fa7ea13e49c47535384085ab89872c737e749
Alexander <Alexander@DESKTOP-R367M2D>
2022-12-08 12:20:06 +0200

    first

---
+++ src/main/java/telran/java2022/KarafkaConsumerApplication.java
@@ -0,0 +1,22 @@
+package telran.java2022;
+
+import java.util.function.Consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
+
+@SpringBootApplication
+public class KarafkaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarafkaConsumerApplication.class, args);
+	}
+	//@Bean
+	//public Consumer<String> cons(){
+	//    return value->System.out.println(); 
+	//}
+
+}
